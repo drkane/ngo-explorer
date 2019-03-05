6cda5a52d0b066f8de98b93c2aef1cb7a5d36688
David Kane <d.r.kane@gmail.com>
2019-03-05 11:35:08 +0000

    homepage improvements

--- ngo_explorer/static/js/home.js
+++ ngo_explorer/static/js/home.js
@@ -1,9 +1,14 @@
 // filter by country
-const countryFilter = document.getElementById('filter-country-list');
-const countryList = document.getElementById('country-list');
-countryFilter.addEventListener('keyup', (event) => {
-    event.preventDefault();
-    const search_term = event.target.value.toLowerCase().replace(/[\W_]+/g, " ");
+
+var updateBasedOnSearch = function (raw_search_term) {
+    const search_term = raw_search_term.toLowerCase().replace(/[\W_]+/g, " ");
+    const countryList = document.getElementById('country-list');
+
+    // update the search term
+    var search = document.getElementById('search-link');
+    document.getElementById('search-term').innerText = raw_search_term;
+    search.href = search.dataset['baseurl'] + "?filter-search=" + search_term;
+
     for (const group of countryList.getElementsByClassName('country-group')) {
         var group_name = group.getElementsByClassName('country-group-name')[0].textContent;
         group_name = group_name.toLowerCase().replace(/[\W_]+/g, " ");
@@ -16,15 +21,22 @@ countryFilter.addEventListener('keyup', (event) => {
 
             if (entry_name.includes(search_term) | group_name.includes(search_term)) {
                 entry.classList.remove("dn");
-                entry.classList.add("di");
+                entry.classList.add("dib");
                 matching_entries += 1;
             } else {
                 entry.classList.add("dn");
-                entry.classList.remove("di");
+                entry.classList.remove("dib");
             }
         }
 
-        if (group_name.includes(search_term) | matching_entries > 0) {
+        if (search_term == "") {
+            group.classList.remove("dn");
+            group_list.classList.remove("dn");
+            group.classList.remove("db");
+            group_list.classList.remove("db");
+            group.classList.add(group.dataset['initialclass']);
+            group_list.classList.add(group.dataset['initialclass']);
+        } else if (group_name.includes(search_term) | matching_entries > 0) {
             group.classList.remove("dn");
             group_list.classList.remove("dn");
             group.classList.add("db");
@@ -36,4 +48,12 @@ countryFilter.addEventListener('keyup', (event) => {
             group_list.classList.remove("db");
         }
     }
+}
+
+updateBasedOnSearch("");
+const countryFilter = document.getElementById('filter-country-list');
+countryFilter.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    updateBasedOnSearch(event.target.value)
 });
+
