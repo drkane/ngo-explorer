5befe9b3221252dd4582819b58afb6d2f5398857
David Kane <d.r.kane@gmail.com>
2019-03-06 12:07:23 +0000

    upload improvements

--- ngo_explorer/static/js/upload.js
+++ ngo_explorer/static/js/upload.js
@@ -7,16 +7,17 @@ const fileUpload = document.getElementById("file-upload");
 fileUpload.addEventListener('change', (event) => {
     event.preventDefault();
     const selectedFile = fileUpload.files[0];
+
+    const datasetTitle = document.getElementById("upload-name");
+    if (datasetTitle.value == "" | datasetTitle.dataset['source'] == "filename"){
+        datasetTitle.value = selectedFile.name.replace(".csv", "");
+        datasetTitle.dataset['source'] = "filename";
+    }
+
     Papa.parse(selectedFile, {
         header: true,
         complete: function (results) {
             document.getElementById("file-upload-columns").innerHTML = '<option value="">Select a column</option>';
-            for (f of results.meta.fields){
-                var option = document.createElement("option");
-                option.setAttribute("value", f);
-                option.innerText = f;
-                document.getElementById("file-upload-columns").append(option);
-            }
 
             document.getElementById("file-upload-columns").addEventListener('change', (e)=>{
                 e.preventDefault();
@@ -31,9 +32,30 @@ fileUpload.addEventListener('change', (event) => {
                         values += 1;
                     }
                 }
+                // charity_numbers = charity_numbers.filter(function (item, i, ar) { return ar.indexOf(item) === i; });
                 document.getElementById("file-upload-output").innerText = `Found ${values} values in field, of which ${charity_numbers.length} look like charity numbers`;
-                document.getElementById("file-charity-numbers").innerText = charity_numbers.join(",\r\n");
+                document.getElementById("charity-numbers-parsed").innerText = charity_numbers.join(",\r\n");
             });
+
+            for (f of results.meta.fields) {
+                var option = document.createElement("option");
+                var selected_option = null;
+                option.setAttribute("value", f);
+                option.classList.add("mw5");
+                option.innerText = f.substring(0, 100);
+                if(f.length > 100){
+                    option.innerText += "...";
+                }
+                if (f.toLowerCase().replace(/\W/g, '').indexOf("charitynumber")!=-1){
+                    selected_option = f;
+                }
+                document.getElementById("file-upload-columns").append(option);
+                if(selected_option){
+                    document.getElementById("file-upload-columns").value = selected_option;
+                    var event = new Event('change');
+                    document.getElementById("file-upload-columns").dispatchEvent(event);
+                }
+            }
 
         }
     });
@@ -41,11 +63,12 @@ fileUpload.addEventListener('change', (event) => {
 
 // test how many values in a charity number field look like charity numbers
 const char_num = document.getElementById("charity-numbers");
-char_num.addEventListener('change', (event)=>{
+char_num.addEventListener('keyup', (event)=>{
     var charity_numbers = event.target.value.match(charity_number_regex);
     if(charity_numbers==null){
         charity_numbers = [];
     }
-    document.getElementById("charity-number-output").innerText = `${charity_numbers.length} values that look like charity numbers.`
+    charity_numbers = charity_numbers.filter(function (item, i, ar) { return ar.indexOf(item) === i; });
+    document.getElementById("charity-number-output").innerText = `Found ${charity_numbers.length} unique values that look like charity numbers.`
     document.getElementById("charity-numbers-parsed").innerText = charity_numbers.join(",\r\n");
 });
